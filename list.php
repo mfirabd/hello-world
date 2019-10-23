5be08e6be9ea0c6176a794a5a63bd3d5014ad572
Mohamad Firdaus Abdullah <39509095+mfirabd@users.noreply.github.com>
2019-10-23 22:11:17 +0800

    my programming journey master list

---
+++ list.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Mfirabd Solutions</title>
+	
+	<!-- Bootstrap -->
+    <link href="gentelella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="gentelella/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container-fluid">
+
+	</div>
+	<h3>Mfirabd Solutions (<i>Laravel Framework 5.5.33</i>)</h3>
+	<dl>
+		<dt>List of Projects:</dt>
+			<dd><a href="csdkpi/">CSDKPI Tracking System</a></dd>
+			<dd><a href="http://localhost/detect_inactive/">Detect Page Inactive</a></dd>
+			<dd>&nbsp;</dd>
+		<dt>Lessons & Trainings:</dt>
+			<dd><a href="http://localhost:8000/products">Classroom 1 with Products</a></dd>
+			<dd><a href="http://localhost:8000/kpis">Classroom 2 with Add KPI capability</a></dd>
+			<dd><a href="">Classroom 3: Admin LTE Jeroennoten</a></dd>
+			<dd><a href="">Classroom 4: Admin LTE Full</a></dd>
+			<dd><a href="">Classroom 5: Gentelella Admin</a></dd>
+			<dd><a href="">Classroom 6: JWT Authentication</a></dd>
+			<dd><a href="">Classroom 7: Authentication using Auth Middleware</a></dd>
+			<dd><a href="http://localhost:8000/eloquent">Classroom 8: Using Eloquent ORM in Laravel</a></dd>
+			<dd><a href="http://localhost:8000/eloquent">Classroom 9: Database</a></dd>
+			<dd>&nbsp;</dd>
+		<dt>Tips & Trickss:</dt>
+			<dd><a href="https://laravel-news.com/laravel-5-4-key-too-long-error">Laravel 5.4: Specified key was too long error</a></dd>
+			<dd><a href="https://milanchheda.com/how-to-redirect-after-registration-or-login-in-laravel">[How-to] Redirect After Registration Or Login in Laravel</a></dd>
+			<dd><a href="http://bootsnipp.com/forms">[TIPS] Build Bootstrap form easily with Bootsnipp</a></dd>
+			<dd><a href="https://www.kirupa.com/html5/detecting_if_the_user_is_idle_or_inactive.htm">Detecting If the User is Idle or Inactive</a></dd>
+			<dd><a href="http://unminify.com/">Free online tool to unminify (unpack, deobfuscate) JavaScript, CSS and HTML code, making it readable and pretty</a></dd>
+			<dd><a href="http://vegibit.com/how-to-get-started-with-eloquent-in-laravel/">Practice Eloquent Queries using Tinker</a></dd>
+			<dd>PHP Sort Multidimensional Array by Key: <code> array_multisort(array_column($array_input, $sort_key), SORT_ASC, $array_input)</code></dd>
+			<dd>&nbsp;</dd>
+		<dt>References:</dt>
+			<dd><a href="https://laracasts.com/skills/laravel">Get Better at Laravel with Laracasts</a></dd>
+			<dd><a href="https://appdividend.com/2017/08/20/laravel-5-5-tutorial-example/">Laravel 5.5 Tutorial With Example From Scratch</a></dd>
+			<dd><a href="https://appdividend.com/2018/02/24/laravel-jwt-authentication-tutorial/">Laravel JWT Authentication Tutorial</a></dd>
+			<dd><a href="https://github.com/almasaeed2010/AdminLTE/issues/1746">AdminLTE Laravel 5.5 install</a></dd>
+			<dd><a href="https://adminlte.io/blog/integrate-adminlte-with-laravel">Integrate AdminLTE with Laravel 5</a></dd>
+			<dd><a href="https://colorlib.com/polygon/gentelella/index.html">Gentelella Admin: Bootstrap admin template preview</a></dd>
+			<dd><a href="https://github.com/puikinsh/gentelella">Gentelella Admin: Bootstrap admin template Official GitHub</a></dd>
+			<dd><a href="https://puikinsh.github.io/gentelella/">Gentelella Alela! - Documentation</a></dd>
+			<dd><a href="https://blog.nasrulhazim.com">Nasrul Hazim: Laravel Training</a></dd>
+			<dd>
+				<ul>
+					<li><a href="https://nasrulhazim.gitbooks.io/laravel-cookbook/content/">https://nasrulhazim.gitbooks.io/laravel-cookbook/content/</a></li>
+					<li><a href="https://www.dropbox.com/sh/948msza3923wqlw/AABu8ipEAutiXx6oZvk6jO-ra?dl=0">https://www.dropbox.com/sh/948msza3923wqlw/AABu8ipEAutiXx6oZvk6jO-ra?dl=0</a></li>
+					<li><a href="http://bit.ly/training-laravel-basic-v4">Basic: http://bit.ly/training-laravel-basic-v4</a></li>
+					<li>Intermediate: (under maintenance)</li>
+					<li><a href="http://bit.ly/training-laravel-dvnc-v1">Advanced: http://bit.ly/training-laravel-dvnc-v1</a></li>
+					<li><a href="http://bit.ly/training-phpunit-dusk">PHPUnit and Dusk: http://bit.ly/training-phpunit-dusk</a></li>
+					<li><a href="http://bit.ly/laratalk-13">http://bit.ly/laratalk-13</a></li>
+					<li><a href="http://bit.ly/laratalk-14">http://bit.ly/laratalk-14</a></li>
+				</ul>
+			</dd>
+			<dd><a href="https://scotch.io/tutorials/a-guide-to-using-eloquent-orm-in-laravel">A Guide to Using Eloquent ORM in Laravel </a></dd>
+			<dd><a href="http://vegibit.com/how-to-get-started-with-eloquent-in-laravel/">How to get started with eloquent in Laravel</a></dd>
+			<dd>&nbsp;</dd>
+		<dt>Tools:</dt>
+			<dd><a href="https://getcomposer.org/">Composer: Dependency Manager for PHP</a></dd>
+			<dd><a href="http://www.drools.org/">Drools: Business Rules Management System (BRMS)</a></dd>
+			<dd><a href="https://github.com/bower/bower">Bower - A package manager for the web</a></dd>
+			<dd><a href="http://www.wisemapping.com/">WiseMapping - Free online mind mapping editor for individuals and business</a></dd>
+			<dd>&nbsp;</dd>
+		<dt>Notes:</dt>
+			<dd>Open GitCMD</dd>
+			<dd>Execute <i>composer create-project --prefer-dist laravel/laravel AppName</i></dd>
+			<dd>browse to <i>AppName/public</i></dd>
+			<dd>execute <i>bower install gentelella --save</i></dd>
+	</dl>
+
+	<!-- jQuery -->
+    <script src="gentelella/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="gentelella/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+
